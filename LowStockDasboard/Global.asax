95d4653d4cb74cc27dd3402dd7fef1e19e575ee5
Richard Iyama <richardiyama@yahoo.com>
2017-07-20 10:35:57 +0100

    first commit

---
+++ LowStockDasboard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LowStockDasboard.Global" Language="C#" %>
---
+++ LowStockDasboard/Models/Locations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LowStockDasboard.Models
+{
+    public class Location
+    {
+        public string LocationId;
+        public string LocationName;
+    }
+}
